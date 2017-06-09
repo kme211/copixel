aff79729b2e1d6f97ba6a99ac7d3a370bc3f111f
Keari Eggers <kungfu.keari@gmail.com>
2017-06-09 17:09:30 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const javascript = {
+  test: /\.(js)$/, 
+  use: [{
+    loader: 'babel-loader',
+    options: { presets: ['es2015', 'react'] }
+  }],
+};
+
+const uglify = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
+  compress: { warnings: false }
+});
+
+const config = {
+  entry: {
+    // we only have 1 entry, but I've set it up for multiple in the future
+    App: './src/javascripts/index.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'src', 'public', 'js'),
+    filename: '[name].bundle.js'
+  },
+
+  module: {
+    rules: [javascript]
+  }
+};
+
+process.noDeprecation = true;
+
+module.exports = config;
