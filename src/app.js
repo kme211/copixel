dbf685128b31e84055962998bd33f01ab5bf0215
Keari Eggers <kungfu.keari@gmail.com>
2017-06-16 15:40:59 -0500

    add prettier

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const api = require('./api');
-const errorHandlers = require('./handlers/errorHandlers');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const api = require("./api");
+const errorHandlers = require("./handlers/errorHandlers");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api', api);
+app.use("/api", api);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(errorHandlers.developmentErrors);
 }
 
